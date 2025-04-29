1b7ef4cf16b98d77ba551f0ffb6e500516f1bb8e
Ralph <laurenceventura0w0@gmail.com>
2025-04-29 10:43:53 +0800

    Added insert token on user click

--- main.py
+++ main.py
@@ -13,13 +13,17 @@ def extractSprite(sheet, x, y, scaleSize, size):
     sprite = pygame.transform.scale(sprite, scaleSize)
     return sprite
 
-# Main Game Class
+# Handles the Main game loop and grid logic
 class FlipSOS:
     def __init__(self):
         pygame.init()
-        self.resolution = (1280, 720)
+        self.resolution = (1080, 720)
         self.screen = pygame.display.set_mode(self.resolution)
         pygame.display.set_caption('FlipSOS')
+        
+        self.playerO = 'O'
+        self.playerS= 'S'
+        self.currentPlayer = 'S'
         
         self.rows = 8
         self.columns = 8
@@ -41,10 +45,17 @@ class FlipSOS:
             if event.type == pygame.QUIT:
                 self.running = False
             
-            # For debugging, print the board
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 1:
+                # For debugging, print the board
+                if event.button == 3:
                     self.grid.printBoard()
+                
+                # Place a token based on player input
+                if event.button == 1:
+                    x, y = pygame.mouse.get_pos()
+                    x, y = (x - self.tokenSize[0]) // self.tokenSize[0], (y - self.tokenSize[1]) // self.tokenSize[1]
+                    self.grid.addToken(self.grid.gridLogic, self.currentPlayer, y, x)
+                    self.currentPlayer = self.playerO if self.currentPlayer == self.playerS else self.playerS
                 
     def update(self):
         pass
@@ -53,15 +64,18 @@ class FlipSOS:
         self.screen.fill((173, 216, 230)) # RRGGBB Format
         self.grid.drawGrid(self.screen)
         pygame.display.update()
-        
+
+# Handles the grid design and logic       
 class Grid:
     def __init__(self, rows, columns, tokenSize, gameClass): 
         self.gameClass = gameClass
         self.y = rows
         self.x = columns
         self.tokenSize = tokenSize
-        self.sToken = loadImage('assets/S.png', tokenSize)
-        self.oToken = loadImage('assets/O.png', tokenSize)
+        self.resizedToken = (tokenSize[0] - 2, tokenSize[1] - 2)
+        self.sToken = loadImage('assets/S.png', self.resizedToken)
+        self.oToken = loadImage('assets/O.png', self.resizedToken)
+        self.tokens = {}
         self.bgDict = self.loadBackgroundImages()
         self.bg = self.createBackground()
         self.gridLogic = self.regenGrid(self.y, self.x)
@@ -110,18 +124,49 @@ class Grid:
         for y in range(rows):
             line = []
             for x in range(columns):
-                line.append(0)
+                line.append('-')
             grid.append(line)
+            
+        # Add starting tokens
+        self.addToken(grid, 'O', 3, 3)
+        self.addToken(grid, 'S', 3, 4)
+        self.addToken(grid, 'O', 4, 4)
+        self.addToken(grid, 'S', 4, 3)
 
         return grid
     
     def drawGrid(self, displayWindow):
         displayWindow.blit(self.bg, (0, 0))
+        
+        for token in self.tokens.values():
+            token.draw(displayWindow)
     
     def printBoard(self):
         # Prints the grid to the console for debugging
+        print('Current Board:')
         for row in self.gridLogic:
             print(row)
+            
+    def addToken(self, grid, player, y, x):
+        tokenImage = self.oToken if player == 'O' else self.sToken
+        self.tokens[(y, x)] = Token(player, y, x, self.tokenSize, tokenImage, self.gameClass)
+        grid[y][x] = self.tokens[(y, x)].player
+            
+class Token:
+    def __init__(self, player, gridX, gridY, tokenSize, image, gameClass):
+        self.player = player
+        self.gridX = gridX
+        self.gridY = gridY
+        self.posX = tokenSize[0] + (gridY * tokenSize[0])
+        self.posY = tokenSize[1] + (gridX * tokenSize[1] + 4)
+        self.gameClass = gameClass
+        self.image = image
+        
+    def transition(self):
+        pass
+    
+    def draw(self, displayWindow):
+        displayWindow.blit(self.image, (self.posX, self.posY))
 
 if __name__ == '__main__':
     game = FlipSOS()
